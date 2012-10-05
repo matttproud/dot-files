716e759c175d2e867252474f5ab4bac27603b21e
Matt T. Proud <matt@soundcloud.com>
2012-10-05 08:33:55 +0200

    Removal of ``desktop-mode``.

--- emacs.d/personal/session.el
+++ emacs.d/personal/session.el
@@ -14,25 +14,6 @@
 
 (trace-enter "session.el")
 
-(require 'desktop)
-
-(setq desktop-load-locked-desktop t ; Always load the locked desktop.
-      desktop-save t ; Always save the desktop, no matter what state the file
-                     ; was last in.
-      desktop-dirname "~/.emacs.d/desktop"
-      desktop-base-file-name "emacs.desktop"
-      desktop-base-lock-name "lock"
-      desktop-path (list desktop-dirname))
-
-(desktop-save-mode t)
-
-(require 'midnight)
-
-
-(setq history-length 250)
-(add-to-list 'desktop-globals-to-save 'file-name-history)
-(add-hook 'auto-save-hook (lambda () (desktop-save-in-desktop-dir)))
-
 (provide 'session)
 
 (trace-exit "session.el")
