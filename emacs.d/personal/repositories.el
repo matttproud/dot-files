c5dbdd9949cdbf200fe42612409f9371bf1b4d5c
Matt T. Proud <matt.proud@gmail.com>
2013-05-20 13:43:53 +0200

    Add Marmalade repositories.

--- emacs.d/personal/repositories.el
+++ emacs.d/personal/repositories.el
@@ -12,45 +12,8 @@
 ;;
 ;; -*- mode: lisp -*-
 
-(trace-enter "personal.el")
-
-(setq dependencies
-     '(behavior bindings buffer bytecompile display identity
-       python repositories))
-
-(setq hostname-components (split-string (system-name) "\\."))
-(setq google-found nil)
-(while hostname-components
-  (setq momentary (car hostname-components))
-  (print momentary)
-  (setq hostname-components (cdr hostname-components))
-  (if (string= momentary "google") (setq google-found t)))
-
-
-(defun load-google-stuffs
-  ()
-  "Load Google items."
-  (message "Loading Google Stuffs.")
-  (setq dependencies
-    (append dependencies '(google))))
-
-(if google-found
-  (load-google-stuffs))
-
-(mapcar
- (lambda (x)
-   (setq file (format "~/.emacs.d/personal/%s.el" x))
-   (load-file file))
- dependencies)
-
-(setq requires
-      '(bookmark desktop))
-
-(mapcar
- (lambda (x)
-   (require x))
- requires)
-
-(provide 'personal)
-
-(trace-exit "personal.el")
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
